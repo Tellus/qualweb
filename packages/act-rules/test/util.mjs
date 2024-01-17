c4cedf2e8ee489616d6af7af278f0b59c5a32375
lindhart <johannes@the.homestead.dk>
2024-01-17 21:55:55 +0100

    test(act-rules): add function `createStaticFileServer()`
    A *very* simple static file server. This is necessary at the moment, because the `Dom#process()` function in @qualweb/dom doesn't seem to play well with "file://" addresses.

--- packages/act-rules/test/util.mjs
+++ packages/act-rules/test/util.mjs
@@ -1,4 +1,7 @@
 import puppeteer from 'puppeteer';
+import http from 'node:http';
+import fs from 'node:fs';
+import path from 'node:path';
 
 export async function launchBrowser() {
   const args = [];
@@ -9,5 +12,43 @@ export async function launchBrowser() {
   return await puppeteer.launch({
     headless: process.env.TEST_PUPPETEER_HEADLESS == false || true,
     args,
+  });
+}
+
+/**
+ * Creates a tiny static file server to host any files under a specific
+ * directory.
+ * @param {string} rootPath Root of path to host files from.
+ * @returns 
+ */
+export function createStaticFileServer(rootPath) {
+  const contentTypeMappings = {
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.jpeg': 'image/jpg',
+  }
+
+  return http.createServer((request, response) => {
+    const filePath = path.resolve(rootPath, request.url.substring(1));
+
+    if (!fs.existsSync(filePath)) {
+      response.writeHead(404);
+      response.end(`No such file: ${filePath}`);
+
+      return;
+    }
+
+    const headers = {};
+
+    if (contentTypeMappings[path.extname(filePath)]) {
+      headers['Content-Type'] = contentTypeMappings[path.extname(filePath)];
+    }
+
+    response.writeHead(200, headers);
+
+    response.end(fs.readFileSync(filePath), 'utf-8');
   });
 }
