fcd4002e0b76b5c91c0692a695d4d973dc2d4f86
lindhart <johannes@the.homestead.dk>
2024-01-17 23:23:54 +0100

    test(act-rules): improve `createStaticFileServer()` and `launchBrowser()`
    # createStaticFileServer:
    - use index.html if a directory is requested.
    - add a bunch more MIME types. I'm hesitant to use an extra dependency for this instead of just a small list.
    - rewrite requests to "/WAI" root to the webroot itself.
    - rewrite contents of files to replace references to w3c.org with their contents. Just realized this approach probably corrupts binary files.
    
    # launchBrowser:
    - the headless test for TEST_PUPPETEER_HEADLESS seemed buggy for the "false" case... maybe it should be "TEST_PUPPETEER_NO_HEADLESS" instead

--- packages/act-rules/test/util.mjs
+++ packages/act-rules/test/util.mjs
@@ -2,6 +2,9 @@ import puppeteer from 'puppeteer';
 import http from 'node:http';
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export async function launchBrowser() {
   const args = [];
@@ -9,8 +12,14 @@ export async function launchBrowser() {
   if (process.env.CI)
     args.push('--no-sandbox');
 
+  const headless = process.env.TEST_PUPPETEER_HEADLESS
+    ? process.env.TEST_PUPPETEER_HEADLESS.toLowerCase() != 'false'
+    : true;
+
+  console.debug(headless);
+
   return await puppeteer.launch({
-    headless: process.env.TEST_PUPPETEER_HEADLESS == false || true,
+    headless,
     args,
   });
 }
@@ -21,34 +30,77 @@ export async function launchBrowser() {
  * @param {string} rootPath Root of path to host files from.
  * @returns 
  */
-export function createStaticFileServer(rootPath) {
+export function createStaticFileServer() {
+  // A lot of these were pulled from https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
   const contentTypeMappings = {
+    '.html': 'text/html',
     '.js': 'text/javascript',
     '.css': 'text/css',
     '.json': 'application/json',
     '.png': 'image/png',
     '.jpg': 'image/jpg',
     '.jpeg': 'image/jpg',
-  }
+    '.mp4': 'video/mp4',
+    '.xml': 'text/xml',
+    '.svg': 'image/svg+xml',
+    '.webm': 'video/webm',
+    '.mp3': 'audio/mpeg',
+  };
 
-  return http.createServer((request, response) => {
-    const filePath = path.resolve(rootPath, request.url.substring(1));
+  const rootPath = path.resolve(__dirname, 'fixtures/w3c-repo/');
+  // /content-assets/wcag-act-rules/
 
-    if (!fs.existsSync(filePath)) {
-      response.writeHead(404);
-      response.end(`No such file: ${filePath}`);
+  return http.createServer(async (request, response) => {
+    // Remove leading slash (to play nice with path.resolve). Also remove "/WAI"
+    // root path. For the W3C site, this points to their subdir on the web
+    // server. For us, it's in the way.
+    const requestPath = request.url.replace(/^\/WAI/, '').replace(/^\//, '');
 
-      return;
+    let filePath = path.resolve(rootPath, requestPath);
+
+    try {
+      const st = await fs.promises.stat(filePath);
+
+      if (st.isDirectory()) {
+        filePath = path.resolve(filePath, 'index.html');
+      } else if (st.isFile() === false) {
+        response.writeHead(400);
+        response.end(`Unknown file type at path ${filePath}`);
+        
+        return;
+      }
+
+      const headers = {};
+
+      if (contentTypeMappings[path.extname(filePath)]) {
+        headers['Content-Type'] = contentTypeMappings[path.extname(filePath)];
+      } else {
+        console.error(`Unknown MIME type for file ${filePath}. Add it to the list!`);
+        response.writeHead(500);
+        response.end('DEV ERROR! Missing mime type for the request file.');
+        return;
+      }
+  
+      response.writeHead(200, headers);
+  
+      let fileContents = fs.readFileSync(filePath, 'utf-8');
+  
+      // Convert any URLs to w3c.org into absolute paths.
+      fileContents = fileContents.replaceAll('https://www.w3.org', '');
+  
+      response.end(fileContents, 'utf-8');
+    } catch (err) {
+      if (!fs.existsSync(filePath)) {
+        response.writeHead(404);
+        response.end(`No such file: ${filePath}`);
+      } else {
+        response.writeHead(500);
+        response.end(JSON.stringify(err));
+      }
     }
-
-    const headers = {};
-
-    if (contentTypeMappings[path.extname(filePath)]) {
-      headers['Content-Type'] = contentTypeMappings[path.extname(filePath)];
-    }
-
-    response.writeHead(200, headers);
-
-    response.end(fs.readFileSync(filePath), 'utf-8');
   });
 }
+
+// const h = createStaticFileServer();
+
+// h.listen(8081);
