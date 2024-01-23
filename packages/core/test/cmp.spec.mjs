886342756362e90e18f6c94b723daf530b867468
Johannes L. Borresen <jlb@inqludeit.dk>
2024-01-23 13:20:57 +0100

    test(core): added some unit tests for CMP suppression

---
+++ packages/core/test/cmp.spec.mjs
@@ -0,0 +1,141 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+import { expect } from 'chai';
+import { CMPManager } from '@inqludeit/cmp-b-gone';
+
+import { QualWeb } from '../dist/index.js';
+
+import express from 'express';
+
+export async function setupStaticServer(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const app = new express();
+    
+      app.use(express.static(path));
+  
+      app.listen(process.env.EXPRESS_PORT, function () {
+        resolve(this);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+describe('CMP suppression', function () {
+  this.timeout(10000);
+
+  // QualWeb instance. Kept out of test cases so teardown can guarantee cleanup.
+  let qw = null;
+
+  let staticServer;
+  let staticServerHost;
+
+  // Set up a static file server for serving an HTML file with cookies. This
+  // has to be done, because most browsers will not work with cookies on HTML
+  // files that are clearly served from local disk.
+  before(async () => {
+    staticServer = await setupStaticServer(path.resolve(__dirname, 'fixtures/cmp'));
+
+    staticServerHost = `http://localhost:${staticServer.address().port}`;
+  });
+
+  // Close the static file server after tests have run.
+  after(() => {
+    staticServer.close();
+  })
+
+  // Set up a fresh QualWeb instance before each test. This is kept outside of
+  // the test case itself so we can run a separate teardown, even in cases where
+  // the tests crash.
+  beforeEach(async () => {
+    if (qw !== null) {
+      throw new Error(`qw variable has already been defined. Did a previous test case fail to clean up?`);
+    }
+
+    qw = new QualWeb();
+  });
+
+  afterEach(async () => {
+    if (qw === null) {
+      throw new Error(`qw cannot be cleaned up, it is null!`);
+    }
+
+    await qw.stop();
+
+    qw = null;
+  });
+
+  it('Should correctly detect a CMP defined in the built-in collection', async () => {
+    const url = `${staticServerHost}/cookiesite.html`;
+
+    await qw.start(undefined, { headless: 'new' }, {
+      cmpManager: true,
+    });
+
+    const reports = await qw.evaluate({
+      url,
+      log: {
+        file: false,
+        console: false,
+      },
+    });
+
+    // Ensure report was generated for the URL. This means that the CMP was
+    // correctly suppressed.
+    expect(reports).to.have.property(url);
+  });
+
+  it('Should evaluate a page with a CMP if no descriptors are set (trivial pass)', async () => {
+    const url = `${staticServerHost}/cookiesite.html`;
+
+    await qw.start(undefined, { headless: 'new' }, {
+      cmpManager: await CMPManager.createManager(undefined, false),
+    });
+
+    const reports = await qw.evaluate({
+      url,
+      log: {
+        file: false,
+        console: false,
+      },
+    });
+
+    // Ensure report was generated for the URL. This means that the CMP was
+    // correctly suppressed.
+    expect(reports).to.have.property(url);
+  });
+
+  it('Should fail to evaluate a page with a CMP if no descriptors matched (1 descriptor)', async () => {
+    const url = `${staticServerHost}/cookiesite.html`;
+
+    await qw.start(undefined, { headless: 'new' }, {
+      cmpManager: await CMPManager.createManager(undefined, false),
+    });
+
+    const reports = await qw.evaluate({
+      url,
+      log: {
+        file: false,
+        console: false,
+      },
+      cmpDescriptors: [
+        {
+          storageOptions: {
+            cookies: ['cconsent', 'consentIDandDate']
+          },
+          presenceSelectors: ['div#bad-selector'],
+          acceptAllSelectors: ['button.bad-selector']
+        }
+      ],
+    });
+
+    // Ensure report was NOT generated for the URL. This means that evaluation
+    // failed. We're assuming it was because the CMP was not detected.
+    expect(reports).to.not.have.property(url);
+  });
+});
