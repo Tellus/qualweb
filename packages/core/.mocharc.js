871023757e11c7c7acb5d83896945d1c3ba5c466
Johannes L. Borresen <jlb@inqludeit.dk>
2024-01-26 12:45:01 +0100

    test(core): add .mocharc.js file
    Makes it easier to add complexity to tests down the line. Also, makes the tests show up in Mocha Test Explorer for VSCode (and probably others), making it easier to get an overview on tests.

---
+++ packages/core/.mocharc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "color": true,
+  "ui": "bdd",
+  // "spec": "test/**/*.spec.mjs",
+  // "spec": "test/**/*.spec.ts",
+  // "require": [
+  //   // "tsconfig-paths/register",
+  //   "ts-node/register",
+  // ],
+  // Uncomment to get a JSON file with test results instead of output in console.
+  // "reporter": "json",
+  // "reporterOptions": [
+  //   "output=./results.json",
+  // ],
+}
